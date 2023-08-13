dde9769b6c7a8b4618dac3953d5d7c949738037a
Amr Magdy <70772544+3amo-magdy@users.noreply.github.com>
2023-08-13 11:53:22 +0300

    Update params.py

--- params.py
+++ params.py
@@ -19,17 +19,18 @@ class StopWordsOptions(Enum):
 # }
 
 MARBERT = {
-    training_args:{
-       do_train: True, evaluate_during_training: True,
-       adam_epsilon: 1e-8,
-       learning_rate: 2e-5,
-       warmup_steps: 0,
-       per_device_train_batch_size: 16,
-       per_device_eval_batch_size: 16,
-       num_train_epochs: 1,
-       logging_steps: 50,
-       save_steps: 400,
-       seed: 42,
-       report_to: "",
+    'training_args': {
+        'do_train': True,
+        'evaluate_during_training': True,
+        'adam_epsilon': 1e-8,
+        'learning_rate': 2e-5,
+        'warmup_steps': 0,
+        'per_device_train_batch_size': 16,
+        'per_device_eval_batch_size': 16,
+        'num_train_epochs': 1,
+        'logging_steps': 50,
+        'save_steps': 400,
+        'seed': 42,
+        'report_to': "",
     }
 }
